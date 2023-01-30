9ac800caa72fa2bca58b3f88f3d87ebe0831bee8
ktadique <117470787+ktadique@users.noreply.github.com>
2023-01-30 00:22:50 +0000

    fixed empty input and repeated search bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,6 @@ display past city current forecast
 //-----------------
 
 //moment.js
-let currentHour = moment().hour();
 let currentDay = moment().format("DD/MM/YY");
 
 let todayDiv = $("#today");
@@ -37,16 +36,25 @@ function findCity(event) {
     method: "GET",
   }).then(function (response) {
     console.log(response); //Test if correct
+    //render main forecast
     renderForecast(response);
-    /* //render 5-day forcast
-    futureForecast(response); */
-    futureForecast();
+    //render 5-day forecast
+    renderFutureForecast(response);
   });
-  //generate button into city history
-  renderCityButtons();
-}
 
+  //If input is empty or has already been searched throw error
+  if (city === "") {
+    alert("Please enter a city name");
+  } else if (cityName.includes(city)) {
+    alert("Please enter a new city name");
+  } else {
+    //generate button into city history
+    renderCityButtons();
+  }
+}
+//Main city forecast
 function renderForecast(response) {
+  let currentForecast = response.list[0];
   //create main forecast elements
   let todayCard = $("<div>").attr("class", "card");
   let todayCardBody = $("<div>").attr("class", "card-body");
@@ -56,65 +64,74 @@ function renderForecast(response) {
   let cityHeading = $("<h4>").attr("class", "font-weight-bold");
   cityHeading.text(`${response.city.name}  (${currentDay})`);
   //temp
-  let currentForcast = response.list[0];
-  let kelToCel = currentForcast.main.temp - 273.15;
+  let kelToCel = currentForecast.main.temp - 273.15;
   let celcius = Math.round(kelToCel * 10) / 10;
   let todayTemp = $("<p>");
   todayTemp.text(`Temp: ${celcius}°C`);
   //weather icon
   let icon = $("<img>");
-  let fcIcon = currentForcast.weather[0].icon;
+  let fcIcon = currentForecast.weather[0].icon;
   icon.attr(
     { src: `http://openweathermap.org/img/wn/${fcIcon}@2x.png` },
-    { alt: `${currentForcast.weather[0].description}` },
+    { alt: `${currentForecast.weather[0].description}` },
     { width: "50" },
     { height: "50" }
   );
   cityHeading.append(icon);
   //wind
   let todayWind = $("<p>");
-  todayWind.text(`Wind: ${currentForcast.wind.speed} KPH`);
+  todayWind.text(`Wind: ${currentForecast.wind.speed} KPH`);
   //humidity
   let todayHumi = $("<p>");
-  todayHumi.text(`Humidity: ${currentForcast.main.humidity}%`);
+  todayHumi.text(`Humidity: ${currentForecast.main.humidity}%`);
 
+  //append elements
   todayDiv.append(todayCard);
   todayCard.append(todayCardBody);
   todayCardBody.append(cityHeading, todayTemp, todayWind, todayHumi);
 }
 
 //5-Day forecast
-function futureForecast() {
+
+function renderFutureForecast(response) {
   let forecastHeading = $("<h4>").attr("class", "font-weight-bold");
   forecastHeading.text("5-Day Forecast:");
   let forecastDeck = $("<div>").attr("class", "card-deck");
   forecastDiv.append(forecastHeading, forecastDeck);
 
+  /*   let forecastList = response.list;
+  //filter next 5 days
+  let forecastListDates = `${moment()
+    .add(i + 1, "d")
+    .format("YYYY-MM-DD")} 12:00:00`;
+
+  //filter through response to retrieve a certain time - using midday to test
+  let filteredDates = forecastList.filter(function midDay(list) {
+    return list.dt_txt === forecastListDates;
+  });
+  console.log(filteredDates[0]); */
+
   for (let i = 0; i < 5; i++) {
-    //create forecast cards
-    let forecastCard = $("<div>").attr("class", `card fc-${i}`);
+    let forecastCard = $("<div>").attr("class", "card");
     let forecastCardBody = $("<div>").attr("class", "card-body");
 
     //forecast card information
-    let nextDay = moment().add(i, "d");
-    let forecastDate = $("<h5>");
-    forecastDate.text(nextDay.format("DD/MM/YY"));
-    let forecastTemp = $("<p>");
-    forecastTemp.text(`Temp: `);
-    let forecastWind = $("<p>");
-    forecastWind.text(`Wind: `);
-    let forecastHumi = $("<p>");
-    forecastHumi.text(`Humidity: `);
+    let forecastDate = moment()
+      .add(i + 1, "d")
+      .format("DD/MM/YY");
+    let futureDate = $("<h5>");
+    futureDate.text(forecastDate);
+    let futureTemp = $("<p>");
+    futureTemp.text(`Temp: `);
+    let futureWind = $("<p>");
+    futureWind.text(`Wind: `);
+    let futureHumi = $("<p>");
+    futureHumi.text(`Humidity: `);
 
     //append info to cards, cards to forecast deck
     forecastDeck.append(forecastCard);
     forecastCard.append(forecastCardBody);
-    forecastCardBody.append(
-      forecastDate,
-      forecastTemp,
-      forecastWind,
-      forecastHumi
-    );
+    forecastCardBody.append(futureDate, futureTemp, futureWind, futureHumi);
   }
 }
 
@@ -123,55 +140,34 @@ function renderCityButtons() {
   //push input into cityName array
   cityName.push(input);
 
+  //empty ul to make sure that buttons do not duplicate
   cityHistory.empty();
 
   //create a button for every item in array
   for (i = 0; i < cityName.length; i++) {
     let cityButton = $("<li>");
-
-    cityButton.text(cityName[i]);
-
     cityButton.addClass("btn btn-outline-dark mb-1 city-btn");
-
     cityButton.attr("data-city", cityName[i]);
-
+    cityButton.text(cityName[i]);
     cityHistory.append(cityButton);
   }
 }
 
 //Retrieve past searches when city button is pressed
 function getPastForcast(event) {
-  let input = $(event.target).attr("data-city");
-  alert("you clicked " + input); //test
+  let city = $(event.target).attr("data-city");
+  // alert("you clicked " + city); //test
   let apiKey = "&appid=690c3a6b7201e389fe103be085cb462f";
-  let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${input}${apiKey}`;
+  let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}${apiKey}`;
 
   todayDiv.empty();
-  forecastDiv.empty();
   $.ajax({
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
-    let currentForcast = response.list[0];
-    let kelvin = currentForcast.main.temp;
-    let celcius = `${kelvin - 273.15}\u00B0C`;
-
-    let todayCard = $("<div>").attr("class", "card");
-    let todayCardBody = $("<div>").attr("class", "card-body");
-    let cityHeading = $("<h4>").attr("class", "font-weight-bold");
-    cityHeading.text(`${input}  ${currentDay}`);
-    let todayTemp = $("<p>");
-    todayTemp.text(`Temp: ${celcius}`);
-    let todayWind = $("<p>");
-    todayWind.text(`Wind: ${currentForcast.wind.speed} KPH`);
-    let todayHumi = $("<p>");
-    todayHumi.text(`Humidity: ${currentForcast.main.humidity}%`);
-
-    todayDiv.append(todayCard);
-    todayCard.append(todayCardBody);
-    todayCardBody.append(cityHeading, todayTemp, todayWind, todayHumi);
+    renderForecast(response);
+    futureForecast(response);
   });
-  futureForecast();
 }
 
 //event listeners
